83da848695094a31c6ca4166647b30bf32fffef6
timqian <timqian@t9t.io>
2021-08-20 15:39:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+
+async function updateSheet() {
+	// Initialize the sheet - doc ID is the long id in the sheets URL
+	const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+	// await doc.useServiceAccountAuth(require('./headllines-github-aaedb908d840.json'));
+	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+	await doc.useServiceAccountAuth({
+	  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+	  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+	});
+	
+	await doc.loadInfo(); // loads document properties and worksheets
+	console.log(doc);
+	// await doc.updateProperties({ title: 'renamed doc' });
+	
+	// const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+	// console.log(sheet.title);
+	// console.log(sheet.rowCount);
+	
+	// // adding / removing sheets
+	// const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
+	// await newSheet.delete();
+}
+
+updateSheet()
