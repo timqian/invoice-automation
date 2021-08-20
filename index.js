952c5afb6988e7076113efb2d45e2c4020823418
timqian <timqian@t9t.io>
2021-08-21 00:43:00 +0800

    update script

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ const today = dayjs().format('MMM DD, YYYY');
 const InvoiceNo = dayjs().format('YYYY-MM') + '-TQ';
 
 async function updateSheet() {
-	// Initialize the sheet - doc ID is the long id in the sheets URL
-	const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-	// await doc.useServiceAccountAuth(require('./headllines-github-aaedb908d840.json'));
-	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-	await doc.useServiceAccountAuth({
-		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-		// Ref: https://github.com/theoephraim/node-google-spreadsheet/issues/244#issuecomment-616031349
-		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-	});
+  // Initialize the sheet - doc ID is the long id in the sheets URL
+  const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+  // await doc.useServiceAccountAuth(require('./headllines-github-aaedb908d840.json'));
+  // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    // Ref: https://github.com/theoephraim/node-google-spreadsheet/issues/244#issuecomment-616031349
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  });
 
-	await doc.loadInfo(); // loads document properties and worksheets
-	// console.log(doc);
-	// await doc.updateProperties({ title: 'renamed doc' });
+  await doc.loadInfo(); // loads document properties and worksheets
+  // console.log(doc);
+  // await doc.updateProperties({ title: 'renamed doc' });
 
-	const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-	console.log('Editing sheet:', sheet.title);
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  console.log('Editing sheet:', sheet.title);
 
   await sheet.loadCells();
   // console.log(allCells, sheet);
-	const NumberCell = sheet.getCellByA1('F10');
-	NumberCell.value = InvoiceNo;
+  const NumberCell = sheet.getCellByA1('F10');
+  NumberCell.value = InvoiceNo;
   const DateCell = sheet.getCellByA1('F11');
   DateCell.value = today;
-	await sheet.saveUpdatedCells();
+  await sheet.saveUpdatedCells();
 
   // Download PDF
 
