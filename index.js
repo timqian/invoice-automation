dc3874519673fc7e37a58151b545764e478196e0
timqian <timqian@t9t.io>
2021-08-20 15:50:14 +0800

    update ci

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function updateSheet() {
 	// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
 	await doc.useServiceAccountAuth({
 	  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    // Ref: https://github.com/theoephraim/node-google-spreadsheet/issues/244#issuecomment-616031349
 	  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 	});
 	
